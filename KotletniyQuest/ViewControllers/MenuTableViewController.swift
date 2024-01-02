7693ca13d6bc0113e6ea5f63c36018b842677146
Darncol <60666804+Darncol@users.noreply.github.com>
2024-01-02 14:32:31 +0200

    test

--- KotletniyQuest/ViewControllers/MenuTableViewController.swift
+++ KotletniyQuest/ViewControllers/MenuTableViewController.swift
@@ -19,32 +19,29 @@ final class MenuTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        Set(menu.map{$0.section}).count
+        MealTime.allCases.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         menu.filter{$0.section.rawValue == section}.count
     }
 
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "meal", for: indexPath)
-        
-        let mealCell = cell as! CellForMeal
+        let cell = tableView.dequeueReusableCell(withIdentifier: "meal", for: indexPath) as! CellForMeal
         
         let sectionMeal = menu.filter{
             $0.section.rawValue == indexPath.section
         }[indexPath.row]
         
-        mealCell.mealImage.image = UIImage(named: sectionMeal.pictureURL)
-        mealCell.mealNameLabel.text = sectionMeal.name
-        mealCell.mealPriceLabel.text = "\(sectionMeal.price) RUB"
+        cell.mealImage.image = UIImage(named: sectionMeal.pictureURL)
+        cell.mealNameLabel.text = sectionMeal.name
+        cell.mealPriceLabel.text = "\(sectionMeal.price) RUB"
 
-        return mealCell
+        return cell
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        menu.first(where: {$0.section.rawValue == section})?.section.description
+        MealTime.allCases[section].description
     }
     
 
